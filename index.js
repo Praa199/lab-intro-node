f22dfe8d80648cf7646c508f6deaf9336af8c924
Aldo <aldo.pena.93@hotmail.com>
2021-02-02 19:44:03 +0100

    done until number 7

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class SortedList {
   add(item) {
     this.items.push(item)
     this.length++
-    
+
     this.items.sort(function(a,b){
       return a-b
     }
@@ -15,9 +15,27 @@ class SortedList {
     
   }
 
-  get(pos) {}
+  get(pos) {
+    for (let i = 0; i <  this.items.length; i++) {
+      if(pos > this.items.length || pos == -1){
+        throw new Error('OutOfBounds');
+      }
+       else if(pos ==  this.items[i]){
+      return this.items[i]
+    } 
+    }
+    }
+  
 
-  max() {}
+  max() {
+    let list = []
+    if (list.max([])) {
+      throw new Error('EmptySortedList');
+    } else {
+      list = new SortedList()
+      return list.max()
+    }
+  }
 
   min() {}
 
